f92d2d763ae8f8707181533aaff6bfc0fe48727d
dharmendra689 <pintu98894611@gmail.com>
2023-07-24 12:31:56 +0530

    added emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import './contact.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_pfg8vea', 'template_u08234a',form.current)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <section id='contact'>
-       <h5>Get in Touch</h5>
-       <h2>Contact Me</h2>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
 
-       <div className="container contact__container">
+      <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail  className='contact__option-icon' />
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>E mail</h4>
             <h5>dharmendra44689@gmail.com</h5>
             <a href="mailto:dharmendra44689@gmail.com" target="_blank">Send a Message</a>
@@ -32,14 +45,14 @@ const Contact = () => {
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='Email' placeholder='Your Email' required />
           <textarea name="message" rows={7} placeholder='Your Message' required></textarea>
           <button type='submit' className='btn btn-primary'>Send Message</button>
 
         </form>
-       </div>
+      </div>
     </section>
   )
 }
